525be5929a7ce56a16fbf8ed1ec1643be3af3329
Aldi <aldiaafn16@gmail.com>
2023-09-27 11:02:40 +0700

    update menu side

--- src/assets/component/NavigationMenu.jsx
+++ src/assets/component/NavigationMenu.jsx
@@ -2,7 +2,6 @@ import ImportNavbarSideCard from "../controller/NavbarSideCard"
 import LogoProfile from "../images/image-navbar/staff.png"
 import LogoRetur from "../images/image-navbar/return.png"
 import LogoAdd from "../images/image-navbar/add.png"
-import LogoStock from "../images/image-navbar/warehouse.png"
 import LogoHistory from "../images/image-navbar/history.png"
 import LogoLogout from "../images/image-navbar/logout2.png"
 import LogoCatalog from "../images/image-navbar/writing.png"
@@ -10,20 +9,18 @@ import LogoCatalog from "../images/image-navbar/writing.png"
 
 function LoopMenu() {
     let a = [];
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 6; i++) {
      if(i==0){
        a.push(<ImportNavbarSideCard img={LogoProfile} title="Profil" link="HomePage" />)
     }else if(i==1){
        a.push(<ImportNavbarSideCard img={LogoAdd} title="Pesan" link=""/>)
       }else if(i==2){
          a.push(<ImportNavbarSideCard img={LogoCatalog} title="Catalog" link="HomePage"/>)
-      }else if(i==3){
-        a.push(<ImportNavbarSideCard img={LogoStock} title="Stok" link=""/>)
-      }else if(i==4){
+      } if(i==3){
         a.push(<ImportNavbarSideCard img={LogoRetur} title="Retur" link=""/>)
-   }else if(i==5){
+   }else if(i==4){
         a.push(<ImportNavbarSideCard img={LogoHistory} title="Histori" link=""/>)
-   }else if(i==6){
+   }else if(i==5){
         a.push(<ImportNavbarSideCard dou img={LogoLogout} title="Keluar" link=""/>)
    }
 }
