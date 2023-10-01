1054d6a9c13499915e197788f18e84dd2904876a
Alvin Bernard Wiyono <alvinbwiyono@gmail.com>
2023-10-01 13:36:03 +0700

    excel

--- src/assets/component/Salesman/DataStokBarang.jsx
+++ src/assets/component/Salesman/DataStokBarang.jsx
@@ -1,17 +1,36 @@
-
 import { useEffect, useRef } from "react";
 import $ from "jquery";
 import DataTables from "datatables.net";
-import DataTarget from "../../component/Salesman/DataTarget"
+import DataTarget from "../../component/Salesman/DataTarget";
 import "datatables.net-dt/css/jquery.dataTables.min.css";
 import dataSet from "./dataSet";
+import * as XLSX from "xlsx";
+
+import "datatables.net-buttons";
+import "datatables.net-buttons-dt";
+import "datatables.net-buttons-dt/css/buttons.dataTables.min.css";
+import "datatables.net-buttons/js/buttons.html5.min.js";
+import "datatables.net-buttons/js/buttons.print.min.js";
 
 const Table = () => {
   const tableRef = useRef();
+  const ExportExcel = () => {
+    let Heading = [['ID Barang', 'Nama Barang', 'Stok Karton', 'Stok Pcs','Harga Karton', 'Harga Pcs','Expired']];
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.json_to_sheet(dataSet);
+    XLSX.utils.sheet_add_aoa(ws, Heading);
 
+    //Starting in the second row to avoid overriding and skipping headers
+    XLSX.utils.sheet_add_json(ws,  dataSet, { origin: 'A2', skipHeader: true });
+
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    XLSX.writeFile(wb, 'filename.xlsx');
+  };
   useEffect(() => {
     // Initialize DataTables within the component
-    $(tableRef.current).DataTable({
+
+    new $(tableRef.current).DataTable({
       data: dataSet,
       columns: [
         { title: "ID Barang" },
@@ -22,25 +41,39 @@ const Table = () => {
         { title: "Harga Pcs" },
         { title: "Expired" },
       ],
+      dom: '<"top"lf>rt<"bottom"Bpi>', // Include the buttons in the DOM
+      buttons: [
+        "copy",
+        "csv",
+        {
+          text: "Ecxel",
+          action: ExportExcel,
+        },
+        "pdf",
+        "print", // Specify which buttons to include
+      ],
     });
   }, []);
 
   // Create a reference for the table
   return (
-  <> 
-    <div className="cover flex">
-                <div className="header lg:w-full md:w-1/2 text-primary text-4xl font-semibold">
-                    <p>Katalog</p>
-                </div>
-                <div className="rounded-xl lg:w-1/2 float-right mx-auto text-2xl font-semibold">
-                    <DataTarget/>
-                </div>
-    </div>
-    <div className="cover mt-10" style={{width:"100%"}}>
-        <table className="border-2 border-gray rounded-lg" ref={tableRef}></table>
-    </div>
-  </>
-  )
+    <>
+      <div className="cover flex">
+        <div className="header lg:w-full md:w-1/2 text-primary text-4xl font-semibold">
+          <p>Katalog</p>
+        </div>
+        <div className="rounded-xl lg:w-1/2 float-right mx-auto text-2xl font-semibold">
+          <DataTarget />
+        </div>
+      </div>
+      <div className="cover mt-10" style={{ width: "100%" }}>
+        <table
+          className="border-2 border-gray rounded-lg"
+          ref={tableRef}
+        ></table>
+      </div>
+    </>
+  );
 };
 
 export default Table;
