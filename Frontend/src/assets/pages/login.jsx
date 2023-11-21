808144d58af766c5d2fdbd223bc77d38b7191a8b
Aldi <aldiaafn16@gmail.com>
2023-11-21 20:48:28 +0700

    baru check login

--- Frontend/src/assets/pages/login.jsx
+++ Frontend/src/assets/pages/login.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useEffect } from "react";
+import React, { useEffect, } from "react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { joiResolver } from "@hookform/resolvers/joi";
@@ -14,10 +14,9 @@ import client from "../controller/client";
 
 export default function Loginfunction() {
     //Animasi Aos ===================
+
     useEffect(() => {
         AOS.init();
-        // Disable text selection for elements
-        // with class "no-select"
         const noSelectElements = document.querySelectorAll(".selectdisable");
         noSelectElements.forEach((element) => {
             element.style.webkitUserSelect = "none";
@@ -30,17 +29,9 @@ export default function Loginfunction() {
 
     // Pindah halaman ================
     const Navigate = useNavigate();
-    // const handleRedirect = () => {
-    //     Navigate("./CatalogBarang")
-    // }
     //Pindah halaman =================
 
     //Pengecekan Joi =================
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    });
-    const [errors, setErrors] = useState({});
     const schema = Joi.object({
         username: Joi.string().email({ tlds: { allow: false } }).required().messages({
             "string.empty": "Email Tidak boleh kosong",
@@ -48,39 +39,42 @@ export default function Loginfunction() {
         }),
         password: Joi.string()
             .alphanum()
-            .message("* Password hanya boleh mengandung huruf dan angka")
-            .required()
+            .messages({
+                "string.empty": "Password Tidak boleh kosong",
+            }).required()
     });
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const validation = schema.validate(formData, { abortEarly: false });
-        if (validation.error) {
-            const newErrors = {};
-            validation.error.details.forEach((detail) => {
-                newErrors[detail.path[0]] = detail.message;
-            });
-            setErrors(newErrors);
-        } else {
-            // Kirim data ke server atau lakukan tindakan lainnya
-            console.log('Data berhasil divalidasi:', formData);
-            try {
-                let temp = await client.post("/api/login", {
-                    email: formData.username,
-                    password: formData.password
+
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm({
+        resolver: joiResolver(schema),
+    });
+
+    async function login(data) {
+        try {
+            let temp = await client.post("/api/login", {
+                email: data.username,
+                password: data.password
+            })
+            Navigate(`${temp.data.jabatan.replace(/\s/g, '')}`)
+        } catch (error) {
+            if (error.response.data === "Email tidak terdaftar") {
+                setError("username", {
+                    type: "custom",
+                    message: 'Email tidak terdaftar'
                 })
-                Navigate(`${temp.data.jabatan.replace(/\s/g, '')}`)
-                //   Navigate("/Salesman")
-            } catch (error) {
-                alert(error.response.data)
+            } else {
+                setError("password", {
+                    type: "custom",
+                    message: 'Password salah'
+                })
             }
         }
-    };
+    }
     //Pengecekan Joi =================
-
 
     return (
         <>
@@ -102,36 +96,31 @@ export default function Loginfunction() {
                                         <div className="text-5xl text-primary justify-center font-semibold">Data Centers</div>
                                         <img src={LogoPerusahaan} className="w-20 h-20 ms-3" alt="" />
                                     </div>
-                                    <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="" method="post">
+                                    <form onSubmit={handleSubmit(login)} className="space-y-4 md:space-y-6" action="" method="post">
                                         <div>
                                             <label
                                                 htmlFor="username"
                                                 className="block mb-1 text-2xl text-primary font-semibold">Email</label>
                                             <input
-                                                value={formData.username}
-                                                onChange={handleChange}
-                                                type="username"
-                                                name="username"
-                                                id="username"
+                                                type="text"
+                                                {...register("username")}
                                                 className="bg-gray-50 border md:h-12 border-primary lg:h-14 text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block lg:w-4/5 md:w-full p-2.5  placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
                                                 placeholder="name@email.com"
-                                                required="" />
-                                            {errors.username && <p className="error text-sm text-primary pt-1">{errors.username}</p>}
+                                            />
+                                            {errors.username && <p className="error text-sm text-primary pt-1">{errors.username.message}</p>}
                                         </div>
                                         <div>
                                             <label
                                                 htmlFor="password"
                                                 className="block mb-1 text-2xl text-primary font-semibold">Password</label>
                                             <input
-                                                value={formData.password}
-                                                onChange={handleChange}
                                                 type="password"
                                                 name="password"
-                                                id="password"
+                                                {...register("password")}
                                                 className="bg-gray-50 border-primary md:h-12 lg:h-14 text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block lg:w-4/5 md:w-full p-2.5  placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
                                                 placeholder="••••••••"
-                                                required="" />
-                                            {errors.password && <p className="error text-sm text-primary pt-1">{errors.password}</p>}
+                                            />
+                                            {errors.password && <p className="error text-sm text-primary pt-1">{errors.password.message}</p>}
                                         </div>
                                         <div className="flex items-center">
                                             <input
@@ -151,7 +140,6 @@ export default function Loginfunction() {
                                             className="lg:w-4/5 md:w-full h-14 text-gray font-medium text-2xl bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-center hover:bg-gray-400 bg-primary hover:text-white">login</button>
                                     </form>
                                 </div>
-
                             </div>
                             {/* //form untuk login ke landing page */}
                             <p className="text-sm lg:text-base text-gray-500 text-center md:pt-4" style={{ marginTop: "72vh" }}>Version 0.1.3</p>
